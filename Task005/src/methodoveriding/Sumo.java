d74446d63591221982bb3922e08cfaace822a746
Jefferson.K <48382453+jeffermagic@users.noreply.github.com>
2019-03-17 22:11:53 -0700

    Update Sumo.java

--- Task005/src/methodoveriding/Sumo.java
+++ Task005/src/methodoveriding/Sumo.java
@@ -1,12 +1,16 @@
 package methodoveriding;
+//creating a parent class
 
 public class Sumo {
 	void run(){System.out.println("Vehicle is running");}
 }
+//creating a child class
 	class Fortuner extends Sumo {
 	 void run(){System.out.println("Car is running safely");}  
 	public static void main(String[] args) {
+		//creating an instance of child class  
 		Fortuner obj =new Fortuner ();
+		//calling the method with child class instance  
 		 obj.run();
 		
 
